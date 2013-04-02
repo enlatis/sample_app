602ee81290547b0e724c559128cf61501002b647
enlatis <peterson.brandon@gmail.com>
2013-04-01 22:45:57 -0700

    RSpec Updates

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -56,5 +56,6 @@ describe "Static pages" do
     it "should have the title 'Contact'" do
       visit '/static_pages/contact'
       page.should have_selector('title', :text => "#{base_title} | Contact")
+    end
   end
 end
